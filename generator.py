07092688303e15e4574f6408449e5ed1aa77b601
Mikhail Sandakov <msandakov@plesk.com>
2023-03-14 12:34:28 +0200

    Make it possible to configure height and width over arguments

--- generator.py
+++ generator.py
@@ -9,12 +9,23 @@ if len(sys.argv) < 2:
 pipeline = StableDiffusionPipeline.from_pretrained(
     "stabilityai/stable-diffusion-2-1",
 )
-pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)  
+pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
 pipeline.enable_attention_slicing()
 pipeline.to("cpu")
 
-
 prompt = sys.argv[1]
-results = pipeline([prompt], num_inference_steps=20, height=1024, width=1024)
-image = results.images[0]
-image.save("my_image.png")
+
+# Todo. Yes I know that non positional arguments are better.
+height = width = 512
+if len(sys.argv) >= 3:
+    height = int(sys.argv[2])
+if len(sys.argv) >= 4:
+    width = int(sys.argv[3])
+
+if height % 16 != 0 or width % 16 != 0:
+    print("Height and width must be divisible by 16.")
+    sys.exit(1)
+
+results = pipeline([prompt], height=height, width=width, num_inference_steps=20, num_images_per_prompt=4)
+for idx, image in enumerate(results.images):
+    image.save("my_image" + str(idx) + ".png")
