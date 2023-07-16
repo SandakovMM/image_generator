0b3d5428b76615e73da7eb86510e53d91b17912e
Mikhail Sandakov <msandakov@plesk.com>
2023-07-16 12:29:34 +0300

    Add cuda support

--- generator.py
+++ generator.py
@@ -3,16 +3,24 @@ from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 from optparse import OptionParser, OptionValueError
 
 
-def generate(prompt, height=512, width=512, output="result.png"):
+def generate(prompt, height=512, width=512, output="result.png", cuda=False):
     pipeline = StableDiffusionPipeline.from_pretrained(
         "stabilityai/stable-diffusion-2-1",
     )
     pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
     pipeline.enable_attention_slicing()
-    pipeline.to("cpu")
+
+    number_of_steps = 20
+    if cuda:
+        pipeline.to("cuda")
+        # We could make more steps when use GPU, in theory results will be better
+        number_of_steps = 30
+    else:
+        pipeline.to("cpu")
 
     results = pipeline([prompt], height=height, width=width,
-                       num_inference_steps=20, num_images_per_prompt=1)
+                       num_inference_steps=number_of_steps,
+                       num_images_per_prompt=1)
     results.images[0].save(output)
 
 
@@ -28,6 +36,7 @@ def main():
     opts.add_option("--height", dest="height", type="int", default=512, help="height of the generated image")
     opts.add_option("--width", dest="width", type="int", default=512, help="width of the generated image")
     opts.add_option("-o", "--output", dest="output", type="string", default="result.png", help="output file name")
+    opts.add_option("--cuda", dest="cuda", action="store_true", default=False, help="use CUDA")
 
     (options, args) = opts.parse_args()
 
@@ -41,7 +50,7 @@ def main():
     if height % 16 != 0 or width % 16 != 0:
         raise OptionValueError("height and width must be divisible by 16")
 
-    generate(prompt, height, width, output=options.output)
+    generate(prompt, height, width, output=options.output, cuda=options.cuda)
 
 
 if __name__ == "__main__":
